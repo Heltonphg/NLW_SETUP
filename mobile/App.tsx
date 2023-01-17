c2190c68cb4394e19e70d883f87d09d6ffe3dcdf
heltonbase <111578214+heltonbase@users.noreply.github.com>
2023-01-17 18:41:08 -0300

    initial configs

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+  useFonts,
+} from "@expo-google-fonts/inter";
+
+import { StatusBar, StyleSheet, Text, View } from "react-native";
+import Loading from "./src/components/Loading";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+  });
+
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>ddddd!</Text>
+      <StatusBar
+        barStyle={"light-content"}
+        backgroundColor="transparent"
+        translucent
+      />
     </View>
   );
 }
@@ -13,8 +36,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#09090A",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#fff",
+    fontFamily: "Inter_700Bold",
   },
 });
---
+++ mobile/src/components/Loading.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { ActivityIndicator, View } from "react-native";
+
+const Loading: React.FC = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#09090A",
+      }}
+    >
+      <ActivityIndicator size="large" color="#7C3AED" />
+    </View>
+  );
+};
+
+export default Loading;
