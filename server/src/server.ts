985434357958a14d9ce7a676f35804e86ff94302
heltonbase <111578214+heltonbase@users.noreply.github.com>
2023-01-17 20:16:52 -0300

    structured folders

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,12 @@
-import fastify from 'fastify';
-import cors from '@fastify/cors';
-import { PrismaClient } from '@prisma/client';
+import cors from "@fastify/cors";
+import fastify from "fastify";
+import { PORT } from "./config/general";
+import { appRoutes } from "./routes";
 
 const app = fastify();
-const prisma = new PrismaClient();
 
 app.register(cors);
-
-const PORT = 3333;
-
-app.get('/hello', async (request, reply) => {
-  const habits = await prisma.habit.findMany();
-  return habits;
-});
+app.register(appRoutes);
 
 app
   .listen({
