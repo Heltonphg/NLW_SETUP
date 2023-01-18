cbbfe9e98584713837396c4eb235b8d65066eacc
heltonbase <111578214+heltonbase@users.noreply.github.com>
2023-01-18 15:35:13 -0300

    style guides

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { View } from "react-native";
-import { Header } from "../../components/Header";
+import React from 'react'
+import { View } from 'react-native'
+import { Header } from '../../components/Header'
 
 export const Home: React.FC = () => {
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
     </View>
-  );
-};
+  )
+}
