8f270d27364fa594e0685b4fc4e8ea9852c098e1
heltonbase <111578214+heltonbase@users.noreply.github.com>
2023-01-18 15:30:06 -0300

    create header

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import { Header } from "../../components/Header";
 
 export const Home: React.FC = () => {
   return (
     <View className="flex-1 bg-background px-8 pt-16">
-      <Text>ddd</Text>
+      <Header />
     </View>
   );
 };
