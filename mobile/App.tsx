7fa5dbb3532574136bace8d82decf242c1a21504
heltonbase <111578214+heltonbase@users.noreply.github.com>
2023-01-18 16:09:27 -0300

    dayjs

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,35 +1,31 @@
+import { StatusBar } from 'react-native'
 import {
   Inter_400Regular,
   Inter_600SemiBold,
   Inter_700Bold,
   Inter_800ExtraBold,
-  useFonts,
-} from "@expo-google-fonts/inter";
-
-import { StatusBar } from "react-native";
-import { Loading } from "./src/components/Loading";
-import { Home } from "./src/screens/Home";
+  useFonts
+} from '@expo-google-fonts/inter'
+import { Loading } from './src/components/Loading'
+import './src/lib/dayjs'
+import { Home } from './src/screens/Home'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_600SemiBold,
     Inter_700Bold,
-    Inter_800ExtraBold,
-  });
+    Inter_800ExtraBold
+  })
 
   if (!fontsLoaded) {
-    return <Loading />;
+    return <Loading />
   }
 
   return (
     <>
       <Home />
-      <StatusBar
-        barStyle={"light-content"}
-        backgroundColor="transparent"
-        translucent
-      />
+      <StatusBar barStyle={'light-content'} backgroundColor="transparent" translucent />
     </>
-  );
+  )
 }
