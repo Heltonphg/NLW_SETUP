b7d82a32966b485571283d2cd7059d4ebbf121b8
heltonbase <111578214+heltonbase@users.noreply.github.com>
2023-01-18 12:00:14 -0300

    create summer days

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,15 @@
+import { Header } from './components/Header'
+import { SummaryTable } from './components/SummaryTable'
 import './styles/global.css'
 
-function App() {
+export function App() {
   return (
-    <div className="bg-zinc-600 w-10 h-10 text-white rounded flex items-center justify-center">
-      ola
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+
+        <SummaryTable />
+      </div>
     </div>
   )
 }
-
-export default App
