78157fab9a1821a617116579b47c3b0107bf8416
Helton Wylammi <wylammi.helton@gmail.com>
2023-01-20 11:38:31 -0300

    feat: habit form start

---
+++ mobile/src/@types/navigation.d.ts
@@ -0,0 +1,11 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      Home: undefined
+      new: undefined
+      habit: {
+        date: string
+      }
+    }
+  }
+}
--- mobile/src/screens/New/index.tsx
+++ mobile/src/screens/New/index.tsx
@@ -1,6 +1,19 @@
 import React from 'react'
-import { View } from 'react-native'
+import { ScrollView, Text, TextInput, View } from 'react-native'
+import { BackButton } from '../../components/BackButton'
 
 export const New: React.FC = () => {
-  return <View />
+  return (
+    <View className="flex-1 bg-background px-4 pt-12">
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <BackButton />
+
+        <Text className="mt-6 text-white font-extrabold text-3xl">Crair Hábito</Text>
+
+        <Text className="mt-6 text-white font-semibold text-base">Qual seu comprometimento?</Text>
+
+        <TextInput className="h-12 pl-4 rounded-lg mt-4 bg-zinc-800 text-white focus: border-2 focus:border-green-600" />
+      </ScrollView>
+    </View>
+  )
 }
