f045d5038302d89a6d531bb1f509b76666d6bb2f
Helton Wylammi <wylammi.helton@gmail.com>
2023-01-20 12:33:45 -0300

    feat: checkbox

---
+++ mobile/src/components/CheckBox/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Text, TouchableOpacity, TouchableOpacityProps, View } from 'react-native'
+import clsx from 'clsx'
+import colors from 'tailwindcss/colors'
+import { Feather } from '@expo/vector-icons'
+
+interface ICheckBoxProps extends TouchableOpacityProps {
+  title: string
+  checked?: boolean
+}
+
+export const CheckBox: React.FC<ICheckBoxProps> = ({ checked = false, title, ...rest }) => {
+  return (
+    <TouchableOpacity className="flex-row mb-2 items-center" {...rest}>
+      <View
+        className={clsx('h-8 w-8 bg-green-500 rounded-lg items-center justify-center', {
+          'bg-zinc-900 rounded-lg': !checked
+        })}>
+        {checked && <Feather name="check" size={20} color={colors.white} />}
+      </View>
+
+      <Text className="text-white text-base ml-3">{title}</Text>
+    </TouchableOpacity>
+  )
+}
--- mobile/src/screens/New/index.tsx
+++ mobile/src/screens/New/index.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ScrollView, Text, TextInput, View } from 'react-native'
 import { BackButton } from '../../components/BackButton'
+import { CheckBox } from '../../components/CheckBox'
+const availableWeekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
 
 export const New: React.FC = () => {
+  const [weekDays, setWeekDays] = useState<number[]>([])
+
+  function handleToggleWeekDay(weekDayIndex: number) {
+    if (weekDays.includes(weekDayIndex)) {
+      setWeekDays((prevState) => prevState.filter((weekDay) => weekDay !== weekDayIndex))
+    } else {
+      setWeekDays((prevState) => [...prevState, weekDayIndex])
+    }
+  }
+
   return (
     <View className="flex-1 bg-background px-4 pt-12">
       <ScrollView showsVerticalScrollIndicator={false}>
@@ -10,9 +22,20 @@ export const New: React.FC = () => {
 
         <Text className="mt-6 text-white font-extrabold text-3xl">Crair Hábito</Text>
 
-        <Text className="mt-6 text-white font-semibold text-base">Qual seu comprometimento?</Text>
+        <Text className="mt-6  text-white font-semibold text-base">Qual seu comprometimento?</Text>
 
-        <TextInput className="h-12 pl-4 rounded-lg mt-4 bg-zinc-800 text-white focus: border-2 focus:border-green-600" />
+        <TextInput className="h-12 pl-4 rounded-lg mt-4 bg-zinc-800 text-white focus: border-2 border-zinc-900 focus:border-green-600" />
+
+        <Text className="mt-4 mb-4 text-white font-semibold text-base">Qual a recorrência?</Text>
+
+        {availableWeekDays.map((day, index) => (
+          <CheckBox
+            key={`${day}-${index}`}
+            title={day}
+            checked={weekDays.includes(index)}
+            onPress={() => handleToggleWeekDay(index)}
+          />
+        ))}
       </ScrollView>
     </View>
   )
