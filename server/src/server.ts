ecf214f149bc1490643efae7bbadd3f6527c15f6
Helton Wylammi <wylammi.helton@gmail.com>
2023-01-21 20:51:11 -0300

    fix: mobile not work if not host:0.0.0.0

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,9 @@
 import cors from "@fastify/cors";
-import fastify from "fastify";
+import Fastify from "fastify";
 import { PORT } from "./config/general";
 import { appRoutes } from "./routes";
 
-const app = fastify();
+const app = Fastify();
 
 app.register(cors);
 app.register(appRoutes);
@@ -11,6 +11,7 @@ app.register(appRoutes);
 app
   .listen({
     port: PORT,
+    host: "0.0.0.0",
   })
   .then(() => {
     console.log(`Server is running on port ${PORT}`);
