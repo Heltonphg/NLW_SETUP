9f3deef290644e863719219788ef1161abe6ee73
Helton Wylammi <wylammi.helton@gmail.com>
2023-01-22 12:06:24 -0300

    refactor:  color adjustment in spotlights

--- web/src/components/NewHabitForm/index.tsx
+++ web/src/components/NewHabitForm/index.tsx
@@ -72,7 +72,7 @@ export const NewHabitForm: React.FC = () => {
         ))}
       </div>
 
-      <button className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500 transition-colors focus:outline-none focus:ring-2 focus:ring-zinc-800 focus:ring-off">
+      <button className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500 transition-colors focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-offset-zinc-900  ">
         <Check size={20} weight="bold" />
         Confirmar
       </button>
