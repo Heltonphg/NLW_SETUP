1012f6332b5ea90ca38c2b32fc268c779961324c
Helton Wylammi <wylammi.helton@gmail.com>
2023-01-22 19:27:16 -0300

    feat: animations

--- mobile/src/components/CheckBox/index.tsx
+++ mobile/src/components/CheckBox/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { Text, TouchableOpacity, TouchableOpacityProps, View } from 'react-native'
+import { Text, TouchableOpacity, TouchableOpacityProps } from 'react-native'
+import Animated, { ZoomIn, ZoomOut } from 'react-native-reanimated'
 import clsx from 'clsx'
 import colors from 'tailwindcss/colors'
 import { Feather } from '@expo/vector-icons'
-
 interface ICheckBoxProps extends TouchableOpacityProps {
   title: string
   checked?: boolean
@@ -12,12 +12,14 @@ interface ICheckBoxProps extends TouchableOpacityProps {
 export const CheckBox: React.FC<ICheckBoxProps> = ({ checked = false, title, ...rest }) => {
   return (
     <TouchableOpacity className="flex-row mb-2 items-center" {...rest}>
-      <View
+      <Animated.View
         className={clsx('h-8 w-8 bg-green-500 rounded-lg items-center justify-center', {
           'bg-zinc-900 rounded-lg': !checked
-        })}>
+        })}
+        entering={ZoomIn}
+        exiting={ZoomOut}>
         {checked && <Feather name="check" size={20} color={colors.white} />}
-      </View>
+      </Animated.View>
 
       <Text className="text-white text-base ml-3 font-semibold">{title}</Text>
     </TouchableOpacity>
--- mobile/src/components/ProgressBar/index.tsx
+++ mobile/src/components/ProgressBar/index.tsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { View } from 'react-native'
-
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated'
 interface iProgressBarProps {
   progress?: number
 }
 
 export const ProgressBar: React.FC<iProgressBarProps> = ({ progress = 0 }) => {
+  const sharedProgress = useSharedValue(progress)
+
+  useEffect(() => {
+    sharedProgress.value = withTiming(progress)
+  }, [progress])
+
+  const styles = useAnimatedStyle(() => {
+    return {
+      width: `${sharedProgress.value}%`
+    }
+  })
+
   return (
     <View className="w-full h-3 rounded-xl bg-zinc-700 mt-4">
-      <View
-        className="h-3 rounded-xl bg-violet-600"
-        style={{
-          width: `${progress}%`
-        }}></View>
+      <Animated.View className="h-3 rounded-xl bg-violet-600" style={styles}></Animated.View>
     </View>
   )
 }
